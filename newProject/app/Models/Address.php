a95f25718bc12796b31b2065179a196b17984507
pawaritBai <pawaritthow@gmail.com>
2025-03-22 20:17:04 +0700

    สามารถ เอา address ของลูกค้ามาได้แล้ว

--- newProject/app/Models/Address.php
+++ newProject/app/Models/Address.php
@@ -10,4 +10,12 @@ class Address extends Model
     protected $primaryKey = 'AddressID';
     protected $guarded = ['AddressID'];
     protected $fillable = ['Province', 'District', 'Subdistrict', 'PostalCode', 'HouseNumber', 'Street', 'CreatedAt'];
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'customer_id', 'user_id');
+    }
+
 }
